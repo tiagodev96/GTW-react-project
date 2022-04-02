3de98bf2fe1289fcaf415c0388064f3a1a5328e9
Tiago Batista <tiago.dev96@gmail.com>
2022-04-01 23:49:09 -0300

    removing useless materialicons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,7 @@ function Navbar() {
     <header>
       <nav>
         <ul className="nav-list">
-          <li className="nav-item tutorial">
-            <span class="material-icons">question_mark</span>
-          </li>
           <li className="nav-item logo">Guess The Word</li>
-          <li className="nav-item statistics">
-            <span class="material-icons">insert_chart_outlined</span>
-          </li>
         </ul>
       </nav>
     </header>
